731996dfc296ec74b94a6a7c0525b3bd1c5f2322
jw <jiangwen.0918@qq.com>
2020-06-02 17:33:59 +0800

    新增注册页面

--- 202005/learn_1/1_env/Scripts/learn_login/login/views.py
+++ 202005/learn_1/1_env/Scripts/learn_login/login/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, redirect
 from . import models
 from . import forms
+import hashlib
+
+
+def hash_code(s, salt='mysite'):
+    h = hashlib.sha256()
+    s += salt
+    h.update(s.encode())
+    return h.hexdigest()
 
 
 def index(request):
-    pass
+    if not request.session.get('is_login', None):
+        return redirect('/login/')
     return render(request, 'login/index.html')
 
 
 def login(request):
+    if request.session.get('is_login', None):  # 不允许重复登录
+        return redirect('/index/')
     if request.method == 'POST':
         login_form = forms.UserForm(request.POST)
         message = '请检查填写的内容！'
@@ -18,47 +29,70 @@ def login(request):
 
             try:
                 user = models.User.objects.get(name=username)
-            except:
+            except :
                 message = '用户不存在！'
                 return render(request, 'login/login.html', locals())
 
-            if user.password == password:
+            if user.password == hash_code(password):
+                request.session['is_login'] = True
+                request.session['user_id'] = user.id
+                request.session['user_name'] = user.name
                 return redirect('/index/')
             else:
                 message = '密码不正确！'
                 return render(request, 'login/login.html', locals())
         else:
             return render(request, 'login/login.html', locals())
+
     login_form = forms.UserForm()
     return render(request, 'login/login.html', locals())
 
-    # if request.method == "POST":
-    #     username = request.POST.get('username')
-    #     password = request.POST.get('password')
-    #     # print(username, password)
-    #     if username.strip() and password:  # 确保用户名和密码都不为空
-    #         # 用户名字符合法性验证
-    #         # 密码长度验证
-    #         # 更多的其它验证.....
-    #         try:
-    #             user = models.User.objects.get(name=username)
-    #         except:
-    #             message = '用户不存在！'
-    #             return render(request, 'login/login.html', {'message': message})
-    #         if user.password == password:
-    #             print(username, password)
-    #             return redirect('/index/')
-    #         else:
-    #             message = '密码不正确！'
-    #             return render(request, 'login/login.html', {'message': message})
-    # return render(request, 'login/login.html')
-
 
 def register(request):
-    pass
-    return render(request, 'login/register.html')
+    if request.session.get('is_login', None):
+        return redirect('/index/')
+
+    if request.method == 'POST':
+        register_form = forms.RegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():
+            username = register_form.cleaned_data.get('username')
+            password1 = register_form.cleaned_data.get('password1')
+            password2 = register_form.cleaned_data.get('password2')
+            email = register_form.cleaned_data.get('email')
+            sex = register_form.cleaned_data.get('sex')
+
+            if password1 != password2:
+                message = '两次输入的密码不同！'
+                return render(request, 'login/register.html', locals())
+            else:
+                same_name_user = models.User.objects.filter(name=username)
+                if same_name_user:
+                    message = '用户名已经存在'
+                    return render(request, 'login/register.html', locals())
+                same_email_user = models.User.objects.filter(email=email)
+                if same_email_user:
+                    message = '该邮箱已经被注册了！'
+                    return render(request, 'login/register.html', locals())
+
+                new_user = models.User()
+                new_user.name = username
+                new_user.password = hash_code(password1)
+                new_user.email = email
+                new_user.sex = sex
+                new_user.save()
+
+                return redirect('/login/')
+        else:
+            return render(request, 'login/register.html', locals())
+    register_form = forms.RegisterForm()
+    return render(request, 'login/register.html', locals())
 
 
 def logout(request):
-    pass
+    if not request.session.get('is_login', None):
+        return redirect('/login/')
+
+    request.session.flush()
+    # del request.session['is_login']
     return redirect("/login/")
